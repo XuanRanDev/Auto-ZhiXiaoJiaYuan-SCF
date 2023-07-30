dddbb86a155a69a7d4f7513346e2b54ed50fd7ac
XuanRanDev <3135419729@qq.com>
2023-07-30 14:43:33 +0800

    init

---
+++ README.md
@@ -0,0 +1,271 @@
+<div align="center">
+<h1 align="center">
+Auto-ShiXiBeiAn
+</h1>
+<p align="center">
+🥰实习备案自动打卡解决方案🥰
+</p>
+<p align="center">
+支持多用户、自定义位置与时间、微信消息推送
+</br>
+</p>
+</br>
+<a target="_blank" href="https://b23.tv/hsaDxKf">视频教程</a>
+</div>
+</br>
+
+
+
+## 前言
+
+**1、请务必认真阅读此文档后继续！**
+
+**2、在开始之前请帮我点一下右上角的star。**
+
+**3、此项目仅限学习交流，禁止用于任何商业或违法用途！**
+
+**4、此项目仅限学习交流，您必须在Fork或下载此仓库源码后的24小时内删除所有内容。**
+
+**5、此项目仅限学习交流，学习或使用此项目造成任何损失均由个人承担。**
+
+**6、如基于或参考此项目进行二次开发，请注明原作者并使用GPL2.0许可证**
+
+
+
+版权所有：XuanRan
+
+版权所有：XuanRan
+
+版权所有：XuanRan
+
+
+![GitHub release (latest by date including pre-releases)](https://img.shields.io/github/v/release/XuanRanDev/Auto-ShiXiBeiAn?include_prereleases)
+![GitHub](https://img.shields.io/github/license/XuanRanDev/Auto-ShiXiBeiAn)
+![GitHub forks](https://img.shields.io/github/forks/XuanRanDev/Auto-ShiXiBeiAn)
+
+</br>
+
+## 目录
+
+- [前言](#前言)
+- [使用门槛](#使用门槛)
+- [使用方法](#使用方法)
+  - [Github Actions](#github-actions)
+  - [使用自己的服务器部署](#使用自己的服务器部署)
+- [修改自动打卡时间🎯](#修改自动打卡时间)
+- [赞助支持](#赞助支持)
+- [常见问题](#常见问题)
+  - [打卡失败](#打卡失败)
+  - [暂停自动打卡](#暂停自动打卡)
+  - [修改配置文件](#修改配置文件)
+  - [保持最新代码](#保持最新代码)
+
+</br>
+</br>
+
+## 使用门槛
+
+1、帮我点一下右上角的star（星星）
+
+2、仅限个人学习交流使用，禁止任何平台或个人将此项目用于盈利或违法！
+
+
+
+</br></br>
+
+## 使用方法
+
+### Github Actions
+
+推荐指数：⭐⭐⭐⭐⭐
+
+
+优点：适合没有自己服务器的人使用。
+
+
+缺点： 每日打卡时间无法保证十分准时，拥有10-30分钟的误差。
+
+</br>
+
+1.点击Star后Fork本仓库🤪
+![1.png](https://tc.xuanran.cc/2022/12/01/9b1d336235e28.png)
+![2.png](https://tc.xuanran.cc/2022/12/01/2c1c3b427a14a.png)
+</br>
+2.准备配置文件🤔
+
+如果想同时打卡多个用户,请再添加一个数据体就好了([如果还不理解点我](https://github.com/XuanRanDev/Auto-ShiXiBeiAn/wiki))
+
+**注意：配置文件模板下方有配置含义，请务必参照配置含义填写**
+```json
+[
+  {
+    "enable": true,
+    "alias": "张三",
+    "phone": "18888888888",
+    "password": "123456...",
+    "deviceType": "Xiaomi|Mi 13|13",
+    "deviceId": "设备ID",
+    "randomLocation": true,
+    "address": "打卡地址",
+    "longitude": "位置经度",
+    "latitude": "位置纬度",
+    "pushKey": "推送Key"
+  }
+]
+```
+
+其配置含义如下：
+
+| 参数名称 | 含义                                                         |
+| -- | ------------------------------------------------------------ |
+| enable | 是否启用该用户的打卡（true或false)                           |
+| alias | 别名，用于在打卡日志中标识不同用户，可空。                   |
+| phone | 手机号                                                       |
+| password | 密码                                                         |
+| deviceType | 设备类型,格式:手机品牌英文名称\|手机代号\|安卓系统版本,例如:Xiaomi\|Mi 13\|13 |
+| deviceId | 设备ID,36位字母+数字组合,[点我获取随机ID](http://did.sxba.xuanran.cc)          |
+| randomLocation   | 打卡位置浮动，启用后每次打卡系统将会在你原有的经纬度的基础之上删掉最后一位数字并随机加入一位数字，使每次打卡经纬度不同      |
+| address | 打卡地址,例如中国河南省洛阳市xxxxx                           |
+| longitude | 打卡位置经度,通过坐标拾取来完成，[传送门](https://jingweidu.bmcx.com/) |
+| latitude | 打卡位置纬度,通过坐标拾取来完成，[传送门](https://jingweidu.bmcx.com/) |
+| pushKey | 打卡结果微信推送，微信推送使用的是pushPlus，请到官网绑定微信([传送门](https://www.pushplus.plus/))，然后在发送消息里面把你的token复制出来粘贴到pushKey这项 |
+
+
+
+
+
+</br>
+
+3.配置Secret
+
+填写完成后请复制如上配置文件，然后打开仓库的Settings->Secrets->Actions->New repository secret
+
+Name填USERS
+
+Secret填改好的配置文件
+
+![3.png](https://tc.xuanran.cc/2022/11/13/2143b390f8199.png)
+![5.png](https://tc.xuanran.cc/2022/12/01/36cadab52b21b.png)
+
+4.运行测试
+
+**以下部分图片复用了工学云自动打卡，除左上角forked from xxxx外与视频教程无其他区别**
+![5.png](https://tc.xuanran.cc/2022/11/13/500e789b3dfec.png)
+![6.png](https://tc.xuanran.cc/2022/11/13/1366e5e0ced97.png)
+![7.png](https://tc.xuanran.cc/2022/11/13/2a2b4b7e01884.png)
+![8.png](https://tc.xuanran.cc/2022/11/13/bd1cd3218f77a.png)
+![9.png](https://tc.xuanran.cc/2022/11/13/33c6cec2e37ec.png)
+![4.png](https://tc.xuanran.cc/2022/12/01/735c10732d2e0.png)
+</br></br></br></br>
+
+至此，自动打卡将会在每天8点左右自动运行打卡。😉
+
+
+</br></br></br>
+
+### 使用自己的服务器部署
+
+推荐指数：⭐⭐⭐⭐⭐
+
+优点：运行稳定、准时。
+
+缺点：有一定的上手成本。
+
+具体教程：
+
+1、下载本仓库源码到你服务器。
+
+2、在服务器中安装好Python环境。
+
+3、运行命令来下载依赖。
+
+```python
+pip install requests
+pip install pytz
+pip install pycryptodome
+```
+
+4、在百度搜索：你的操作系统+ 定时任务，查看如何创建定时任务。
+
+5、创建一个user.json配置文件在项目目录，并将配置文件放入其中
+
+6、运行python Main.py测试
+
+</br></br></br>
+
+
+## 修改自动打卡时间🎯	
+
+修改自动打卡时间需要了解Cron表达式的使用。😴
+**修改打卡时间不要开浏览器翻译！！**
+
+
+</br>
+1.编辑sign.yml文件，找到图中我圈出的部分
+
+![image-20221021093411661](https://tc.xuanran.cc/2022/11/10/5d81dcc0bff46.png)
+
+</br>
+
+2.编辑表达式
+
+GitHub的cron表达式不支持精准到秒，所以从最左边开始，分别为：
+
+分钟 小时 日 月份 星期
+
+而且Github的服务器时间会比我们晚八个小时，所以在你需要打卡的时间-8配置到里面就行了
+
+</br>
+
+例如说在上午十点打卡就是:
+
+```yml
+- cron: "00 02 * * *"
+```
+
+**请注意，在修改打卡时间尽量不要设置在运行高峰期，例如说八点整九点整等，运行高峰期会造成任务启动有极高延迟，可设置为7点57分等非整点时间。**
+
+</br>
+</br>
+
+
+
+
+
+## 赞助支持
+
+如果此仓库帮助了你学到了新知识，你可以帮我买杯可乐，但如果你使用此项目产生任何盈利，请不要支持。
+
+![赞助支持](https://tc.xuanran.cc/2022/11/20/b8f5ddc944634.png)
+
+
+## 常见问题
+
+### 打卡失败
+如果遇到打卡失败，并且错误信息中含有HTTP、timed out字样的，原因在于连接实习备案服务器超时，目前没有很好的解决办法，经过测试，此错误出现率不高，如果遇到可手动重新运行下Actions。
+
+### 暂停自动打卡
+在Actions-Sign-右边三个点disable workflow
+</br>
+
+### 修改配置文件
+Settings-Secrets-Actions-下面Repository secrets有个USERS，点击小箭头编辑，里面没内容是正常的，配置文件一旦保存将无法再被看到。
+</br>
+
+### 保持最新代码
+随着职校家园的更新，自动打卡可能会在某个版本后失效，开发者会及时更新代码，但你Fork的代码并无法保证与主分支（我的代码）实时同步，此时需要手动同步代码，需要注意的是Github不会有任何通知告诉你代码过时或有新版发布，为此，你可以扫描下方二维码关注公众号，当有重要更新或调整时会在公众号发文提醒。
+
+手动同步代码方法：
+![微信截图_20221130142844.png](https://tc.xuanran.cc/2022/11/30/1dad103cbcd0a.png)
+
+公众号：
+![e87a1043ea8f4fada3bb99ba8e35767.jpg](https://tc.xuanran.cc/2022/12/02/d1b00d4d20886.jpg)
+
+
+
+
+
+</br></br>
+最后，帮我点下仓库的小星星，Thanks.
+
+😀😀😀😀😀
---
+++ index.py
@@ -0,0 +1,199 @@
+"""
+版权所有：XuanRan
+微信：XuanRan_Dev
+邮箱：XuanRanDev@qq.com
+公众号：XuanRan
+"""
+
+
+import base64
+import json
+import os
+import random
+from hashlib import md5
+
+import requests
+from requests.adapters import HTTPAdapter
+
+from utils import MessagePush
+
+requests.adapters.DEFAULT_RETRIES = 10
+pwd = os.path.dirname(os.path.abspath(__file__)) + os.sep
+
+s = requests.session()
+s.mount('http://', HTTPAdapter(max_retries=10))
+s.mount('https://', HTTPAdapter(max_retries=10))
+s.keep_alive = False
+
+headers = {
+    "os": "android",
+    "phone": "Xiaomi|Mi 13|12",
+    "appVersion": "52",
+    "Sign": "Sign",
+    "cl_ip": "192.168.1.90",
+    "User-Agent": "okhttp/3.14.9",
+    "Content-Type": "application/json;charset=utf-8"
+}
+
+"""
+加密算法
+"""
+copy_right = "CgoK5byA5Y+R6ICF77yaWHVhblJhbgog5b6u5L+" \
+             "h77yaWHVhblJhbl9EZXYKIOiBlOezu+mCrueuse" \
+             "+8mlh1YW5SYW5EZXZAcXEuY29tCiDlpoLmnpzkv" \
+             "aDmmK/kubDmnaXnmoTmraTpobnnm67ku6Pooajk" \
+             "vaDooqvpqpfkuobvvIHkvaDlj6/ku6XliqDkuIr" \
+             "miJHlvq7kv6Hkuobop6Pmm7TlpJrkv6Hmga/jgI" \
+             "IKCuatpOmhueebruW8gOa6kOWcsOWdgOS4uu+8m" \
+             "mh0dHBzOi8vZ2l0aHViLmNvbS9YdWFuUmFuRGV2" \
+             "L0F1dG8tWmhpWGlhb0ppYVl1YW4KCuWmguaenOa" \
+             "tpOmhueebruW4ruWKqeWIsOS6huS9oO+8jOS9oO" \
+             "WPr+S7peivt+aIkeWWneadr+WltuiMtu+8mgoKP" \
+             "GltZyBzcmM9Imh0dHBzOi8vdGMueHVhbnJhbi5j" \
+             "Yy8yMDIyLzExLzIwL2I4ZjVkZGM5NDQ2MzQucG5" \
+             "nIiAvPgoKCuWFrOS8l+WPt++8mgo8aW1nIHNyYz" \
+             "0iaHR0cHM6Ly90Yy54dWFucmFuLmNjLzIwMjIvM" \
+             "TIvMDIvZDFiMDBkNGQyMDg4Ni5qcGciIC8+"
+
+
+def getMd5(text: str):
+    return md5(text.encode('utf-8')).hexdigest()
+
+
+def parseUserInfo():
+    allUser = ''
+    if os.path.exists(pwd + "user.json"):
+        print('找到配置文件，将从配置文件加载信息！')
+        with open(pwd + "user.json", encoding="utf-8") as f:
+            lines = f.readlines()
+            for line in lines:
+                allUser = allUser + line + '\n'
+    else:
+        return json.loads(os.environ.get("USERS", ""))
+    return json.loads(allUser)
+
+
+def save(user, uid, token):
+    url = 'http://sxbaapp.zcj.jyt.henan.gov.cn/interface/clockindaily20220827.ashx'
+
+    longitude = user["longitude"]
+    latitude = user["latitude"]
+    if user["randomLocation"]:
+        longitude = longitude[0:len(longitude) - 1] + str(random.randint(0, 10))
+        latitude = latitude[0:len(latitude) - 1] + str(random.randint(0, 10))
+
+    data = {
+        "dtype": 1,
+        "uid": uid,
+        "address": user["address"],
+        "phonetype": user["deviceType"],
+        "probability": -1,
+        "longitude": longitude,
+        "latitude": latitude
+    }
+    headers["Sign"] = getMd5(json.dumps(data) + token)
+    res = requests.post(url, headers=headers, data=json.dumps(data))
+
+    if res.json()["code"] == 1001:
+        return True, res.json()["msg"]
+    return False, res.json()["msg"]
+
+
+def getToken():
+    url = 'http://sxbaapp.zcj.jyt.henan.gov.cn/interface/token.ashx'
+    res = requests.post(url, headers=headers)
+    if res.json()["code"] == 1001:
+        return True, res.json()["data"]["token"]
+    return False, res.json()["msg"]
+
+
+def login(user, token):
+    password = getMd5(user["password"])
+    deviceId = user["deviceId"]
+
+    data = {
+        "phone": user["phone"],
+        "password": password,
+        "dtype": 6,
+        "dToken": deviceId
+    }
+    headers["Sign"] = getMd5((json.dumps(data) + token))
+    url = 'http://sxbaapp.zcj.jyt.henan.gov.cn/interface/relog.ashx'
+    res = requests.post(url, headers=headers, data=json.dumps(data))
+    return res.json()
+
+
+def prepareSign(user):
+    if not user["enable"]:
+        print(user['alias'], '\u672a\u542f\u7528\u6253\u5361\uff0c\u5373\u5c06\u8df3\u8fc7')
+        return
+
+    print('已加载用户', user['alias'], '\u5373\u5c06\u5f00\u59cb\u6253\u5361')
+
+    headers["phone"] = user["deviceType"]
+
+    res, token = getToken()
+    if not res:
+        print('用户', user['alias'], '\u83b7\u53d6\u0054\u006f\u006b\u0065\u006e\u5931\u8d25')
+        MessagePush.pushMessage('\u804c\u6821\u5bb6\u56ed\u6253\u5361\u5931\u8d25！',
+                                '\u804c\u6821\u5bb6\u56ed\u6253\u5361\u83b7\u53d6\u0054\u006f\u006b\u0065\u006e\u5931\u8d25\uff0c\u9519\u8bef\u539f\u56e0\uff1a' +
+                                token +
+                                decode_base64(copy_right),
+                                user["pushKey"])
+        return
+
+    loginResp = login(user, token)
+
+    if loginResp["code"] != 1001:
+        print('\u7528\u6237', user['alias'], '\u767b\u5f55\u8d26\u53f7\u5931\u8d25\uff0c\u9519\u8bef\u539f\u56e0\uff1a', loginResp["msg"])
+        MessagePush.pushMessage('\u804c\u6821\u5bb6\u56ed\u767b\u5f55\u5931\u8d25！', '\u804c\u5c4f\u5bb6\u56ed\u767b\u5f55\u5931\u8d25\uff01' + loginResp["msg"] + decode_base64(copy_right), user["pushKey"])
+        return
+
+    uid = loginResp["data"]["uid"]
+    resp, msg = save(user, uid, token)
+
+    if resp:
+        print(user["alias"], '\u6253\u5361\u6210\u529f\uff01')
+        MessagePush.pushMessage('\u804c\u6821\u5bb6\u56ed\u6253\u5361\u6210\u529f\uff01！',
+                                '\u7528\u6237\uff1a' + user["phone"] +
+                                '\u804c\u6821\u5bb6\u56ed\u6253\u5361\u6210\u529f\uff01' +
+                                decode_base64(copy_right), user["pushKey"])
+        return
+    print(user["alias"], "\u6253\u5361\u5931\u8d25")
+    MessagePush.pushMessage('\u804c\u6821\u5bb6\u56ed\u6253\u5361\u5931\u8d25',
+                            '\u7528\u6237\uff1a' +
+                            user["phone"] +
+                            '\u804c\u6821\u5bb6\u56ed\u6253\u5361\u5931\u8d25\u0021\u539f\u56e0\u003a' +
+                            msg +
+                            decode_base64(copy_right),
+                            user["pushKey"])
+
+
+def encode_base64(string):
+    encoded_bytes = base64.b64encode(string.encode('utf-8'))
+    encoded_string = encoded_bytes.decode('utf-8')
+    return encoded_string
+
+
+def decode_base64(encoded_string):
+    decoded_bytes = base64.b64decode(encoded_string)
+    decoded_string = decoded_bytes.decode('utf-8')
+    return decoded_string
+
+
+def main_handler(a, b):
+    users = parseUserInfo()
+
+    for user in users:
+        try:
+            prepareSign(user)
+        except Exception as e:
+            print('\u804c\u6821\u5bb6\u56ed\u6253\u5361\u5931\u8d25\uff0c\u9519\u8bef\u539f\u56e0\uff1a' + str(e))
+            MessagePush.pushMessage('\u804c\u6821\u5bb6\u56ed\u6253\u5361\u5931\u8d25',
+                                    '\u804c\u6821\u5bb6\u56ed\u6253\u5361\u5931\u8d25\u002c' +
+                                    '\u5177\u4f53\u9519\u8bef\u4fe1\u606f\uff1a' + str(e) + decode_base64(copy_right)
+                                    , user["pushKey"])
+
+
+if __name__ == '__main__':
+    main_handler(1, 2)
